4632408c03b2169701290ce6c5ebf1954567ce24
Pavel Trofimov <r-half@yandex.ru>
2021-10-31 21:37:27 +0300

    Initial commit

---
+++ src/main/java/ru/fischerski/knigdomsEp2/Main.java
@@ -0,0 +1,12 @@
+package ru.fischerski.knigdomsEp2;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(Kata.translate("***lo w***d!", new String[]{"hello", "world"}));
+//        System.out.println(Kata.translate("*ow ****v* **** ****u oq**y *t***. s*opq. qro***, q*x", new String[]{"ooqqu","ptqqq","qqqovq","qpqq","qpx","oqqqy","qropoo","sqopq","qow"}));
+//        System.out.println(Kata.translate("", new String[]{"hell", "weak"}));
+//        System.out.println(Kata.translate("***", new String[]{"mel", "dell"}));
+//        System.out.println(Kata.translate("c**l, w*ak!", new String[]{"hell", "cell", "week", "weak"}));
+        System.out.println(Kata.translate("****. ***,", new String[]{"aaa", "bbbb"}));
+    }
+}
